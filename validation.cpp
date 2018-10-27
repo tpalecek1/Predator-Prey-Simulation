48e885390aef71d1687bcf59b8d277e3228c92c9
tpalecek1 <36023933+tpalecek1@users.noreply.github.com>
2018-10-27 11:30:06 -0700

    Add files via upload
    Added main menu

---
+++ validation.cpp
@@ -0,0 +1,106 @@
+/*********************************************************************
+** Program name: War With Dice
+** Author: Timothy Palecek
+** Date: 10/3/2018
+** Description: This file stores validation functions that ensures inputs are of the desired
+**				type and within the desired range, and returns these values.
+*********************************************************************/
+
+#include "validation.hpp"
+#include <string>
+#include <ctime>
+#include <iostream>
+#include <limits>
+
+//Checks that user input only contains alphanumeric values
+std::string alphaNumValidation(std::string message) {
+	//get input from user
+	std::string strInput = " ";
+	std::getline(std::cin, strInput);
+
+	//check that user did not just press enter
+	while (strInput == "") {
+		std::cout << message;
+		std::getline(std::cin, strInput);
+	}
+	//Check if input contains only alphanumeric characters
+	bool isInt = true;
+	for (int i = 0; i < strInput.length(); i++) {
+		if (!isalnum(strInput[i]) && strInput[i] != ' ') {
+			isInt = false;
+		}
+	}
+	//Keep checking if re-entered input is integer within desired range
+	while (isInt == false) {
+		isInt = true;
+		std::cout << message;
+		std::getline(std::cin, strInput);
+		while (strInput == "") {
+			std::cout << message;
+			std::getline(std::cin, strInput);
+		}
+		for (int i = 0; i < strInput.length(); i++) {
+			if (!isalnum(strInput[i])) {
+				isInt = false;
+			}
+		}
+	}
+
+	return strInput;
+}
+
+
+
+//Check that user input is an integer in the desired range
+int validation(int input, int lowerbound, int upperbound, std::string message) {
+	//get input from user
+	std::string strInput = " ";
+	std::getline(std::cin, strInput);
+
+	//check that user did not just press enter
+	while (strInput == "") {
+		std::cout << message;
+		std::getline(std::cin, strInput);
+	}
+
+	//Check if input is an integer
+	bool isInt = true;
+	for (int i = 0; i < strInput.length(); i++) {
+		if (!isdigit(strInput[i])) {
+			isInt = false;
+		}
+	}
+	//Keep checking if re-entered input is integer within desired range
+	while (isInt == false || std::stoi(strInput) < lowerbound || std::stoi(strInput) > upperbound) {
+		isInt = true;
+		std::cout << message;
+		std::getline(std::cin, strInput);
+		while (strInput == "") {
+			std::cout << message;
+			std::getline(std::cin, strInput);
+		}
+		for (int i = 0; i < strInput.length(); i++) {
+			if (!isdigit(strInput[i])) {
+				isInt = false;
+			}
+		}
+	}
+
+	return std::stoi(strInput);
+}
+
+//Check that user input is a double in the desired range
+double doubleValidation(double lowerbound, double upperbound, std::string message) {
+	double dInput = -2;
+	bool validInput = false;
+	//get input from user
+	std::cin >> dInput;
+	while (!std::cin || dInput < lowerbound || dInput > upperbound) {
+		std::cout << message;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin >> dInput;
+	}
+
+	return dInput;
+}
